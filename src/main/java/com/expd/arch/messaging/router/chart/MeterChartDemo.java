59a3e1e6947cff784f1173db80e482c64db83eb0
Dong Hui <dong.hui@expeditors.com>
2021-03-02 08:08:58 -0800

    covert indents to spaces

--- src/main/java/com/expd/arch/messaging/router/chart/MeterChartDemo.java
+++ src/main/java/com/expd/arch/messaging/router/chart/MeterChartDemo.java
@@ -21,59 +21,9 @@ import org.jfree.ui.RefineryUtilities;
 public class MeterChartDemo {
 
     /**
-     * Displays a meter chart.
-     *
-     * @param value  the value.
-     * @param shape  the dial shape.
-     */
-    void displayMeterChart(double value, DialShape shape) {
-
-        DefaultValueDataset data = new DefaultValueDataset(2000.0);
-        MeterPlot plot = new MeterPlot(data);
-        plot.setUnits("records/minute");
-        plot.setRange(new Range(0.0, 10000.0));
-        plot.setNormalRange(new Range(0.0, 1000.0));
-        plot.setWarningRange(new Range(1000.0, 5000.0));
-        plot.setCriticalRange(new Range(5000.0, 10000.0));
-        
-        plot.setDialShape(shape);
-        plot.setNeedlePaint(Color.white);
-        plot.setTickLabelFont(new Font("SansSerif", Font.BOLD, 9));
-
-        plot.setInsets(new Insets(5, 5, 5, 5));
-        JFreeChart chart = new JFreeChart(
-            "PropertyBasedRouter Throughput", 
-            JFreeChart.DEFAULT_TITLE_FONT, 
-            plot, 
-            false
-        );
-
-        MeterLegend legend = new MeterLegend("Sample Meter");
-        chart.setLegend(legend);
-
-        chart.setBackgroundPaint(new GradientPaint(0, 0, Color.white, 0, 1000, Color.blue));
-
-        JFrame chartFrame = new ChartFrame("PropertyBasedRouter Throughput", chart);
-        chartFrame.addWindowListener(new WindowAdapter() {
-          /**
-           * Invoked when a window is in the process of being closed.
-           * The close operation can be overridden at this point.
-           */
-          public void windowClosing(WindowEvent e) {
-            System.exit(0);
-          }
-        });
-        chartFrame.pack();
-        RefineryUtilities.positionFrameRandomly(chartFrame);
-        chartFrame.setSize(400, 400);
-        chartFrame.setVisible(true);
-
-    }
-
-    /**
      * Starting point for the meter plot demonstration application.
      *
-     * @param args  used to specify the type and value.
+     * @param args used to specify the type and value.
      */
     public static void main(String[] args) {
 
@@ -90,11 +40,9 @@ public class MeterChartDemo {
             int type = Integer.parseInt(args[0]);
             if (type == 0) {
                 dialShape = DialShape.PIE;
-            }
-            else if (type == 1) {
+            } else if (type == 1) {
                 dialShape = DialShape.CIRCLE;
-            }
-            else if (type == 0) {
+            } else if (type == 0) {
                 dialShape = DialShape.CHORD;
             }
         }
@@ -102,6 +50,56 @@ public class MeterChartDemo {
             val = new Double(args[1]).doubleValue();
         }
         h.displayMeterChart(val, dialShape);
+
+    }
+
+    /**
+     * Displays a meter chart.
+     *
+     * @param value the value.
+     * @param shape the dial shape.
+     */
+    void displayMeterChart(double value, DialShape shape) {
+
+        DefaultValueDataset data = new DefaultValueDataset(2000.0);
+        MeterPlot plot = new MeterPlot(data);
+        plot.setUnits("records/minute");
+        plot.setRange(new Range(0.0, 10000.0));
+        plot.setNormalRange(new Range(0.0, 1000.0));
+        plot.setWarningRange(new Range(1000.0, 5000.0));
+        plot.setCriticalRange(new Range(5000.0, 10000.0));
+
+        plot.setDialShape(shape);
+        plot.setNeedlePaint(Color.white);
+        plot.setTickLabelFont(new Font("SansSerif", Font.BOLD, 9));
+
+        plot.setInsets(new Insets(5, 5, 5, 5));
+        JFreeChart chart = new JFreeChart(
+                "PropertyBasedRouter Throughput",
+                JFreeChart.DEFAULT_TITLE_FONT,
+                plot,
+                false
+        );
+
+        MeterLegend legend = new MeterLegend("Sample Meter");
+        chart.setLegend(legend);
+
+        chart.setBackgroundPaint(new GradientPaint(0, 0, Color.white, 0, 1000, Color.blue));
+
+        JFrame chartFrame = new ChartFrame("PropertyBasedRouter Throughput", chart);
+        chartFrame.addWindowListener(new WindowAdapter() {
+            /**
+             * Invoked when a window is in the process of being closed.
+             * The close operation can be overridden at this point.
+             */
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        chartFrame.pack();
+        RefineryUtilities.positionFrameRandomly(chartFrame);
+        chartFrame.setSize(400, 400);
+        chartFrame.setVisible(true);
 
     }
 
