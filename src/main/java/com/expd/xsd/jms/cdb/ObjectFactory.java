79d9295830ac9d8f4b82533e076cafdc73a44210
Dong Hui <dong.hui@expeditors.com>
2021-03-02 08:05:36 -0800

    covert indents to spaces

--- src/main/java/com/expd/xsd/jms/cdb/ObjectFactory.java
+++ src/main/java/com/expd/xsd/jms/cdb/ObjectFactory.java
@@ -15,18 +15,17 @@ import javax.xml.namespace.QName;
 
 
 /**
- * This object contains factory methods for each 
- * Java content interface and Java element interface 
- * generated in the com.expd.xsd.jms.cdb package. 
- * <p>An ObjectFactory allows you to programatically 
- * construct new instances of the Java representation 
- * for XML content. The Java representation of XML 
- * content can consist of schema derived interfaces 
- * and classes representing the binding of schema 
- * type definitions, element declarations and model 
- * groups.  Factory methods for each of these are 
+ * This object contains factory methods for each
+ * Java content interface and Java element interface
+ * generated in the com.expd.xsd.jms.cdb package.
+ * <p>An ObjectFactory allows you to programatically
+ * construct new instances of the Java representation
+ * for XML content. The Java representation of XML
+ * content can consist of schema derived interfaces
+ * and classes representing the binding of schema
+ * type definitions, element declarations and model
+ * groups.  Factory methods for each of these are
  * provided in this class.
- * 
  */
 @XmlRegistry
 public class ObjectFactory {
@@ -35,14 +34,12 @@ public class ObjectFactory {
 
     /**
      * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: com.expd.xsd.jms.cdb
-     * 
      */
     public ObjectFactory() {
     }
 
     /**
      * Create an instance of {@link JMSProviderType }
-     * 
      */
     public JMSProviderType createJMSProviderType() {
         return new JMSProviderType();
@@ -50,7 +47,6 @@ public class ObjectFactory {
 
     /**
      * Create an instance of {@link JMSEnvironmentType }
-     * 
      */
     public JMSEnvironmentType createJMSEnvironmentType() {
         return new JMSEnvironmentType();
@@ -58,7 +54,6 @@ public class ObjectFactory {
 
     /**
      * Create an instance of {@link RouterConfigurationsType }
-     * 
      */
     public RouterConfigurationsType createRouterConfigurationsType() {
         return new RouterConfigurationsType();
@@ -66,7 +61,6 @@ public class ObjectFactory {
 
     /**
      * Create an instance of {@link JAXBElement }{@code <}{@link RouterConfigurationsType }{@code >}}
-     * 
      */
     @XmlElementDecl(namespace = "http://www.expd.com/xsd/jms/cdb", name = "router-configurations")
     public JAXBElement<RouterConfigurationsType> createRouterConfigurations(RouterConfigurationsType value) {
